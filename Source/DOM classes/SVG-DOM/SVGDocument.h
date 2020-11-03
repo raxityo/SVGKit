448d05236cb08719a9e4ab3b0cf1032b48b99136
Raxit Majithiya <rax@expa.com>
2020-11-03 10:11:45 -0700

    Replace <SVGKit/Document.h> with "Document.h"

--- Source/DOM classes/SVG-DOM/SVGDocument.h
+++ Source/DOM classes/SVG-DOM/SVGDocument.h
@@ -1,8 +1,8 @@
 /*
  SVG DOM, cf:
- 
+
  http://www.w3.org/TR/SVG11/struct.html#InterfaceSVGDocument
- 
+
  interface SVGDocument : Document,
  DocumentEvent {
  readonly attribute DOMString title;
@@ -15,7 +15,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import <SVGKit/Document.h>
+#import "Document.h"
 #import "SVGSVGElement.h"
 
 @interface SVGDocument : Document
@@ -34,28 +34,28 @@
 
 /**
  Recursively goes through the document finding all declared namespaces in-use by any tag or attribute.
- 
+
  @return a dictionary mapping "namespace" to "ARRAY of prefix-strings"
  */
 -(NSMutableDictionary*) allPrefixesByNamespace;
 
 /**
  As per allPrefixesByNamespace, but takes the output and guarantees that:
- 
+
  1. There is AT MOST ONE namespace with no prefix
  2. The "prefixless" namespace is the SVG namespace (if possible. This should always be possible for an SVG doc!)
  3. All other namespaces have EXACTLY ONE prefix (if there are multiple, it discards excess ones)
  4. All prefixes are UNIQUE (not used by more than one Namespace)
- 
+
  This is critically important when writing-out an SVG file to disk - As far as I can tell, it's a major ommission from
  the XML Spec (which SVG sits on top of). Without this info, you can't construct the appropriate/correct "xmlns" directives
  at the start of a file.
- 
+
  USAGE INSTRUCTIONS:
- 
+
  1. Call this method to get the complete list of namespaces, including any prefixes used
  2. Invoke Node's "appendXMLToString:..." method, passing-in this output, so it can correctly output prefixes for all nodes and subnodes
- 
+
  @return a dictionary mapping "namespace" to "prefix-string or empty-string for the default namespace"
  */
 -(NSMutableDictionary*) allPrefixesByNamespaceNormalized;
